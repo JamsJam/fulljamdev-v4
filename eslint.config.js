f5d2cc8d72d2f7e2ceeea87afa63366a565cb378
JamsJam <jamsjam971@hotmail.com>
2025-06-06 10:24:50 -0400

    fix: ajout ESlint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    files: ['assets/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      indent: ['error', 2],
+    },
+  },
+];

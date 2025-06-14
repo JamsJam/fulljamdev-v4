2aacc58a3743a8eef8b5b9d4eb7184595cab6417
JamsJam <jamsjam971@hotmail.com>
2025-06-14 10:50:19 -0400

    style : separate css output configuration

---
+++ assets/admin.js
@@ -0,0 +1,9 @@
+//* ======== Import js
+import './bootstrap.js';
+
+//* ======== Import SCSS
+import './styles/admin.scss';
+
+
+
+
--- importmap.php
+++ importmap.php
@@ -20,6 +20,10 @@ return [
         'path' => './assets/entrypoints/home.js',
         'entrypoint' => true,
     ],
+    'admin' => [
+        'path' => './assets/admin.js',
+        'entrypoint' => true,
+    ],
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
